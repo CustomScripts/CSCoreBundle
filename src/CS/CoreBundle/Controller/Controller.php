72893e23ea58582efdeb9bd1ecd28c33c7dcd12b
Pierre du Plessis <pierre@pcservice.co.za>
2012-08-13 09:21:39 +0200

    Initial Commit

---
+++ src/CS/CoreBundle/Controller/Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of the CSCoreBundle package.
+ *
+ * (c) Pierre du Plessis <info@customscripts.co.za>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CS\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller as CoreController;
+
+class Controller extends CoreController {}
