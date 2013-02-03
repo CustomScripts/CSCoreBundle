e3183805f20663b63d2b472d84b47069a40f4288
Pierre du Plessis <pierre@pcservice.co.za>
2013-02-03 17:20:49 +0200

    Added default methods to controller

--- src/CS/CoreBundle/Controller/Controller.php
+++ src/CS/CoreBundle/Controller/Controller.php
@@ -13,4 +13,42 @@ namespace CS\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller as CoreController;
 
-class Controller extends CoreController {}
+class Controller extends CoreController {
+
+	/**
+	 * Return a instance of the doctrine entity manager
+	 *
+	 * @return \Doctrine\Orm\EntityManager
+	 */
+	public function getEm()
+	{
+		// TODO : get entity manager from configuration (So we can use odm entity anagers as well)
+		return $this->get('doctrine.orm.entity_manager');
+	}
+
+	/**
+	 * Adds a message to the session flash
+	 *
+	 * @param string $message The message to add to the session flash
+	 * @param string $type The flash message type (notice, success, error etc)
+	 *
+	 * @return Controller
+	 */
+	public function flash($message, $type = 'notice')
+	{
+		$this->get('session')->getFlashBad()->add($type, $message);
+
+		return $this;
+	}
+
+	/**
+	 * Translates a message
+	 *
+	 * @param string $message
+	 * @return string
+	 */
+	public function trans($message)
+	{
+		return $this->get('translator')->trans($message);
+	}
+}
